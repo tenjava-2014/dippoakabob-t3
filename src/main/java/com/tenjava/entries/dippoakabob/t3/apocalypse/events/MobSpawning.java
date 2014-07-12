6d829cc5b1cf50c7881ab59124e4b604a6eaa214
dippoakabob <thedippoakabob@gmail.com>
2014-07-12 16:26:57 -0700

    Added some friendly messages and a /start command

--- src/main/java/com/tenjava/entries/dippoakabob/t3/apocalypse/events/MobSpawning.java
+++ src/main/java/com/tenjava/entries/dippoakabob/t3/apocalypse/events/MobSpawning.java
@@ -4,13 +4,8 @@ import com.tenjava.entries.dippoakabob.t3.TenJava;
 import com.tenjava.entries.dippoakabob.t3.apocalypse.ApocalypseEvent;
 import com.tenjava.entries.dippoakabob.t3.apocalypse.ApocalypseManager;
 import org.bukkit.Location;
-import org.bukkit.entity.CreatureType;
-import org.bukkit.entity.Creeper;
-import org.bukkit.entity.Entity;
-import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.entity.*;
 import org.bukkit.scheduler.BukkitRunnable;
-
-import java.rmi.server.Skeleton;
 
 /**
  * Created by dippoakabob.
@@ -28,17 +23,18 @@ public class MobSpawning extends ApocalypseEvent {
 
 		new BukkitRunnable() {
 
-			CreatureType[] types = {CreatureType.CAVE_SPIDER, CreatureType.MAGMA_CUBE, CreatureType.SKELETON, CreatureType.ZOMBIE, CreatureType.PIG_ZOMBIE};
+			Class<? extends Entity>[] types = new Class[]{CaveSpider.class, Skeleton.class, Creeper.class, PigZombie.class};
 
 			@Override
 			public void run() {
 				int radius = ApocalypseManager.getRadius();
 				for(int x = -radius; x <= radius; x++) {
 					for (int z = -radius; z <= radius; z++) {
-						Location mobLoc = location.clone().add(x, 0, z);
+						if(TenJava.getRandom().nextInt(MOB_CHANCE) == 0){
+							Location mobLoc = location.clone().add(x, 0, z);
 
-						mobLoc.getWorld().spawnCreature(mobLoc, types[TenJava.getRandom().nextInt(types.length)]);
-
+							mobLoc.getWorld().spawn(mobLoc, types[TenJava.getRandom().nextInt(types.length)]);
+						}
 					}
 				}
 			}
